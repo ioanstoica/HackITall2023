79e555d393b96cc5a1619ebc9241b12f1a1c1a12
Vlad Andries <vlad.andries@code11.com>
2023-04-08 13:55:15 +0300

    add migrations with knex

--- backend/src/infrastructure.js
+++ backend/src/infrastructure.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-require("dotenv").config();
+require("dotenv").config({ path: require("find-config")(".env") });
 
 const config = require("./knexfile");
 const knex = require("knex")(config);
+
 knex.migrate.latest().then(function () {
   // always exit the infrastructure process to swith from the initContainer to the main container
   process.exit();
---
+++ backend/src/migrations/20230408104321_create_users.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  try {
+    return knex.schema.createTable("users", function (table) {
+      table.increments();
+      table.string("name").notNullable();
+      table.string("email").notNullable();
+      table.string("password").notNullable();
+      table.integer("elo").notNullable();
+    });
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
