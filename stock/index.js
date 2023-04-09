d887aedf481f1631526a603046c748744977b881
ioanstoica <ioanstoica89@gmail.com>
2023-04-09 04:20:21 +0300

    stockfish -> stock

---
+++ stock/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const ejs = require('ejs');
+const fs = require('fs');
+const app = express()
+const port = 8001
+
+app.use(express.static('public'))
+
+const template = fs.readFileSync('src/index.ejs', 'utf8');
+const data = { title: 'My Title', message: 'Hello My World!' };
+
+const renderedTemplate = ejs.render(template, data);
+
+app.get('/', (req, res) => {
+   res.send(renderedTemplate)
+})
+
+
+
+let people = ['geddy', 'neil', 'alex'];
+let html = ejs.render('<%= people.join(", "); %>', { people: people });
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
