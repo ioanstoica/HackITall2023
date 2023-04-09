c757a37fec44b52063df0f8a6ff5d1f602af963e
Vlad Andries <vlad.andries@code11.com>
2023-04-09 04:30:24 +0300

    change port for stockfish in Iframe

--- frontend/src/views/game.tsx
+++ frontend/src/views/game.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Iframe from "react-iframe";
 export const Game = ({ gameObj }: any) => {
   console.log(
-    "http://localhost:80/&" + "id=" + gameObj.id + "&fen=" + gameObj.fen
+    "http://localhost:8081/&" + "id=" + gameObj.id + "&fen=" + gameObj.fen
   );
   return (
     <Iframe
       width="100%"
       height="1000px"
       position="absolute"
-      url={"http://localhost:80/&" + "id=" + gameObj.id + "&fen=" + gameObj.fen}
+      url={"http://localhost:8081/&" + "id=" + gameObj.id + "&fen=" + gameObj.fen}
     />
   );
 };
